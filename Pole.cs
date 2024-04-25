f8506b7d43bc3b0892753b02cfd5fa8a5753c39a
Lenovo <Lenovo@DESKTOP-U3NAB9G>
2024-04-25 09:56:46 +0200

    Добавьте файлы проекта.

---
+++ Pole.cs
@@ -0,0 +1,16 @@
+﻿namespace Biblioteka
+{
+    public class Pole
+    {
+        public static double Kwadrat(double a)
+        {
+            double pole;
+            pole = a * a;
+            return pole;
+        }
+        public static double Kolo(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
